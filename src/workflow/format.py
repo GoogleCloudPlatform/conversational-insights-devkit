41e97879db7490e8b5524f703a40b1636511379d
asompalli789 <asompalli@google.com>
2025-07-29 12:28:36 -0700

    Update format.py

--- src/workflow/format.py
+++ src/workflow/format.py
@@ -195,3 +195,20 @@ class Speech:
         """Converts Speech v1 return to list[str]"""
         dict_response = json.loads(type(recognizer_response).to_json(recognizer_response))
         return dict_response
+    
+    def v2_json_to_dict(
+            self,
+            v2_json: dict,
+        ) -> list[str]:
+        """ Converts speech v2 json dict to list[str]"""
+        rr_format_data = {"results":[]}
+        uid = 0
+        for item in v2_json["results"]:
+            if "alternatives" not in item:
+                continue
+            for alternative in item["alternatives"]:
+                if "transcript" not in alternative:
+                    continue
+                rr_format_data["results"].append({"uid":uid,"text":alternative["transcript"]})
+                uid += 1
+        return rr_format_data
